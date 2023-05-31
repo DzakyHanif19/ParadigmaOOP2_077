c43799f46539cf78cf9990684578d15d7a312105
DzakyHanif19 <dzakyannafi1@gmail.com>
2023-05-31 17:40:07 +0700

    memanggil kembali prosedur

---
+++ ProgramNoPewarisan/ProgramNoPewarisan.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class baseClass final {
+public:
+	virtual void perkenalan() {
+		cout << "Hallo saya function dari baseClass";
+	}
+};
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Hallo saya function dari derivedClass";
+	}
+};
+int main() {
+	derivedClass a;
+	a.perkenalan();
+
+	return 0;
+}
